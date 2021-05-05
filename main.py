1882343db9edb582713e2555d7c089066433f354
farabimahmud <farabi@tamu.edu>
2021-05-05 14:20:04 -0500

    added conversion from str to byte and print

--- main.py
+++ main.py
@@ -15,11 +15,30 @@ def process_input(filename):
             elif cipher_type == "Correct":
                 correct_ciphers.append(cipher_text)
     return correct_ciphers, faulty_ciphers
-      
+
+def str_to_bytes(line):
+    arr = [ '' for i in range(16)]
+
+    k = 0
+    for i in range(len(line)):
+        arr[k]= arr[k]+line[i]
+        i = i+1
+        if i%2 == 0:
+            k = k+1 
+    for i in range(16):
+        arr[i] = int(arr[i],base=16)
+    return arr 
+
+def print_state(a):
+    for i in range(4):
+        for j in range(4):
+            print("{:02x}".format(a[i*4+j]), end=" ")
+    print()
 if __name__ == "__main__":
     input_filename = "set1.txt"
     correct_ciphers = []
     faulty_ciphers = []
     correct_ciphers, faulty_ciphers = process_input(input_filename)
     print(len(faulty_ciphers))
-    
+    a = str_to_bytes(correct_ciphers[0])
+    print_state(a)
