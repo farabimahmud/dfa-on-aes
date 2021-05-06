ced04016b39f79390ca12c7e1ab5a14dca42670f
farabimahmud <farabi@tamu.edu>
2021-05-06 03:44:23 -0500

    working task 1 and task 2

--- main.py
+++ main.py
@@ -126,6 +126,42 @@ def task1():
 
 def task2():
     master_key = ''
+    input_filename = "set2.txt"
+    correct_ciphers= []
+    faulty_ciphers = []
+    correct_ciphers, faulty_ciphers = process_input(input_filename)
+
+    # for d in faulty_ciphers:
+    #     print(find_fault_byte_index(correct_ciphers[0],d))
+    # count = 0
+    # for d in faulty_ciphers:
+    #     print(dfa_byte_fault(correct_ciphers[0],d))
+    data = [[] for i in range(4)]
+    c = correct_ciphers[0]
+
+    for i in range(20):
+        d = faulty_ciphers[i]
+        x = dfa_byte_fault(c,d)
+        fault_location = find_fault_byte_index(c,d)
+        if (x is []):
+            continue
+        else:
+            if len(data[fault_location]) == 0:
+                data[fault_location] = np.array(x)
+            else:
+                data[fault_location] = np.append(data[fault_location],x,axis=0)
+
+    guess = np.array([])   
+    for i in range(4):
+        if len(data[i]) != 0:
+            if len(guess)==0:
+                guess = np.array(find_most_common_elem(data[i]))
+                guess = guess.reshape(1,16)
+            else:  
+                guess = np.append(guess,np.array(find_most_common_elem(data[i])).reshape(1,16),axis=0)
+    r10_key = guess.max(axis=0).tolist()
+    master_key = get_master_key_from_recovered(bytes_to_str(r10_key))
+    print("Master Key {}".format(master_key))
     return master_key
 
 def find_fault_byte_index(c,d):
@@ -294,49 +330,5 @@ def find_most_common_elem(mat):
 
 
 if __name__ == "__main__":
-    # task1()
-    # print(bytes_to_str(recovered))
-    # rk = '5468617473206D79204B756E67204675'
-    # print(bytes_to_str(next_rounkdey(str_to_bytes(rk0),rc[0])))
-    # for r in range(0,10,1):
-    #     rk = bytes_to_str(next_rounkdey(str_to_bytes(rk),rc[r]))
-    #     print(r, rk)
-    # test r10 28fddef86da4244accc0a4fe3b316f26
-    # test r0 
-
-    # print(bytes_to_str(prev_roundkey(str_to_bytes(rk1),rc[0])))
-    input_filename = "set2.txt"
-    correct_ciphers= []
-    faulty_ciphers = []
-    correct_ciphers, faulty_ciphers = process_input(input_filename)
-
-    # for d in faulty_ciphers:
-    #     print(find_fault_byte_index(correct_ciphers[0],d))
-    # count = 0
-    # for d in faulty_ciphers:
-    #     print(dfa_byte_fault(correct_ciphers[0],d))
-    data = [[] for i in range(4)]
-    c = correct_ciphers[0]
-
-    for i in range(20):
-        d = faulty_ciphers[i]
-        x = dfa_byte_fault(c,d)
-        fault_location = find_fault_byte_index(c,d)
-        if (x is []):
-            continue
-        else:
-            if len(data[fault_location]) == 0:
-                data[fault_location] = np.array(x)
-            else:
-                data[fault_location] = np.append(data[fault_location],x,axis=0)
-
-    guess = np.array([])   
-    for i in range(4):
-        if len(data[i]) != 0:
-            if len(guess)==0:
-                guess = np.array(find_most_common_elem(data[i]))
-                guess = guess.reshape(1,16)
-            else:  
-                guess = np.append(guess,np.array(find_most_common_elem(data[i])).reshape(1,16),axis=0)
-    key = guess.max(axis=0).tolist()
-    print(key)
+    task1()
+    task2()
